494a60ec8fc622027a3d539626a72356aded5c86
PyCmgMagic <x1247337734@163.com>
2025-10-25 13:11:47 +0800

    去除唐社元素

--- app/layout.tsx
+++ app/layout.tsx
@@ -8,9 +8,8 @@ const geistSans = Geist({ subsets: ["latin"] })
 const geistMono = Geist_Mono({ subsets: ["latin"] })
 
 export const metadata = {
-  title: "唐社长者学院 - 选课平台",
+  title: "长者学院 - 选课平台",
   description: "为老年人打造的简单易用的选课平台",
-    generator: 'v0.app'
 }
 
 export default function RootLayout({
